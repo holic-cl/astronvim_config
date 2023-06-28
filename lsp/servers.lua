db14b3eef841e1d1371e7ddf8ff96e524fda3cb3
Fernando V <holic@protonmail.com>
2023-06-28 00:55:23 -0400

    Switch vetur to volar

---
+++ lsp/servers.lua
@@ -0,0 +1,11 @@
+return {
+  "bashls",
+  "cssls",
+  "dockerls",
+  "jsonls",
+  "phpactor", -- there is also psalm and intelephense as an option for php not sure which is better
+  "pyright", -- there is also pylsp for python, not sure which one you want
+  "tsserver", -- this also will service javascript
+  "volar",
+  "yamlls",
+}
